5fdef42ea8603c17c2d7ccbec725c0c70ece4e1b
megan <megan@siyavula.com>
2018-08-20 23:00:13 +0200

    Restructuring

--- kaggle_credit-risk/develop/04_submit.R
+++
@@ -1,9 +0,0 @@
-# Run model on final app_test data
-TARGET <- predict(model, app_test4, type = "response")
-
-# Create submission file
-submission <- cbind(app_test, TARGET)
-sub_vars <- c("SK_ID_CURR", "TARGET")
-submission <- submission[, sub_vars]
-
-write.csv(submission, "submission.csv", row.names = FALSE)
